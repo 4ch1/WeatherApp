93576618df93ae840f6033a70d43251d0a8a3ad1
4ch1 <farrukh.abdukhalikov@gmail.com>
2019-10-18 16:35:01 +0200

    Add files via upload
    adding project to Github

---
+++ kostra/Windows/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WPFWeather.ViewModel;
+
+namespace WPFWeather.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public WeatherViewModel WeatherViewModel { get; set; }
+       
+    
+        public MainWindow()
+        {
+            WeatherViewModel = new WeatherViewModel();            
+            DataContext = WeatherViewModel;
+            InitializeComponent();
+        }
+       
+        
+        private void ListBoxItem_OnSelected(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            WeatherViewModel viewmodel = new WeatherViewModel();
+            string content = ((System.Windows.Controls.ComboBoxItem)comboBox.SelectedItem).Content as string;
+            if (content != null)
+            {
+                viewmodel.DownloadWeather(content);
+            }
+            else
+                viewmodel.DownloadWeather("Brno");
+
+            DataContext = viewmodel;
+
+        }
+    }
+}
